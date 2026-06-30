b6bd2340552684d1b57868b468251da8c29027e7
Matheus Amaral <161625293+MatheusADC@users.noreply.github.com>
2026-06-30 13:44:19 -0300

    ✨ feat: Acesso da instância FormControl dentro da diretiva

--- src/app/shared/directives/input-validation-directive.ts
+++ src/app/shared/directives/input-validation-directive.ts
@@ -1,13 +1,40 @@
-import { Directive, HostListener, Input } from "@angular/core";
+import { DestroyRef, Directive, HostListener, inject, Input } from "@angular/core";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
+import { NgControl } from "@angular/forms";
 
 @Directive({
   selector: '[appInputValidation]',
 })
 export class InputValidationDirective {
+  private readonly _ngControl = inject(NgControl, { self: true });
+  private readonly _destroyRef = inject(DestroyRef);
+
   @Input() errorMessage = 'Campo inválido';
 
-  ngOnInit() {}
+  ngOnInit() {
+    this._ngControl.statusChanges?.pipe(
+      takeUntilDestroyed(this._destroyRef)
+    ).subscribe(() => {
+      this.updateStatus();
+    });
+  }
 
   @HostListener('blur')
-  onBlur() {}
+  onBlur() {
+    this.updateStatus();
+  }
+
+  updateStatus() {
+    const control = this._ngControl.control;
+
+    if (!control) return;
+
+    const isInvalid = control.invalid && control.touched;
+
+    if (isInvalid) {
+
+    } else {
+
+    }
+  }
 }
